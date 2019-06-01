7c6c267ecf936a4f0052fb91f499bcd727a48569
Vinícius Salmont <vinicius@salmont.ml>
2019-06-01 13:35:40 -0300

    Getting images

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hack19/hack19.dart';
+import 'package:hack19/src/FeedItem.dart' show FeedItem;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,10 +8,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final articles = [];
+  List<FeedItem> articles = [];
 
   @override
   Widget build(BuildContext context) {
+    // this.setState(() => {
+    //       articles:,
+    //     });
+
     return Scaffold(
       appBar: _buildAppBar(),
       backgroundColor: Colors.white,
@@ -59,42 +64,20 @@ class _HomeScreenState extends State<HomeScreen> {
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListView(
-              children: <Widget>[
-                Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Image.network(
-                        'https://cdn-images-1.medium.com/max/1600/1*6xT0ZOACZCdy_61tTJ3r1Q.png',
-                        fit: BoxFit.cover,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 8.0,
-                          left: 8.0,
-                        ),
-                        child: Text(
-                          'TAG',
-                          style: TextStyle(
-                            fontSize: 11.0,
-                            color: Colors.blueGrey,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: <Widget>[
-                            Text('Lorem ipsum set dolor amet'),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            child: ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return _newsCard(
+                  articles[index],
+                );
+              },
             ),
+
+            // ListView(
+            //   children: <Widget>[
+
+            //   ],
+            // ),
           ),
           Column(
             children: <Widget>[Text("Recommendations Page")],
@@ -105,13 +88,49 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       );
 
+  Widget _newsCard(FeedItem item) => Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.network(
+              item.image ??
+                  'https://cdn-images-1.medium.com/max/1600/1*6xT0ZOACZCdy_61tTJ3r1Q.png',
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 8.0,
+                left: 8.0,
+              ),
+              child: Text(
+                'Medium',
+                style: TextStyle(
+                  fontSize: 11.0,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Text(item.title),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+
   void _getMediumPosts() async {
     final fetcher = FeedFetcher('https://medium.com/feed/flutter');
     final feed = await fetcher.fetch();
 
-    print(feed.title);
-    for (var feedItem in feed.items) {
-      print(feedItem.title);
-    }
+    this.setState(() => {
+          articles = feed.items,
+        });
+    // for (var feedItem in feed.items) {
+    //   print(feedItem.title);
+    // }
   }
 }
