e95f5c62973fa3b7b59020d09e9b5e0fd7804eb9
Gabriel Oliveira Rozendo <gabriel.rozendo@gmail.com>
2019-06-01 13:41:45 -0300

    Jobs model + mock

---
+++ lib/Models/Jobs/Job.dart
@@ -0,0 +1,10 @@
+class Job {
+  final String id;
+  final String title;
+  final String description;
+  final String location;
+  final bool canBeRemote;
+  final String url;
+
+  const Job({this.id, this.title, this.description, this.location, this.canBeRemote, this.url});
+}
