6d561c0e8362bded0567ae082084c391bd890fc1
Vinícius Salmont <vinicius@salmont.ml>
2019-06-01 12:22:02 -0300

    Card layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,8 +43,6 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
-        // backgroundColor: Colors.red,
-        // bottom:
       );
 
   Widget _buildBody() => TabBarView(
@@ -62,16 +60,21 @@ class _HomeScreenState extends State<HomeScreen> {
                         fit: BoxFit.cover,
                       ),
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.only(
+                          top: 8.0,
+                          left: 8.0,
+                        ),
                         child: Text(
                           'TAG',
-                          textAlign: TextAlign.left,
-                          // style: TextStyle(),
+                          style: TextStyle(
+                            fontSize: 11.0,
+                            color: Colors.blueGrey,
+                          ),
                         ),
                       ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Text('Lorem ipsum set dolor amet '),
+                        child: Text('Lorem ipsum set dolor amet'),
                       ),
                     ],
                   ),
