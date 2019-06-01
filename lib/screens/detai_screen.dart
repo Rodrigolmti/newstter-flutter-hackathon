8d0fc7f0b9795f7c27232d28f08ef7465b3c03b6
Vinícius Salmont <vinicius@salmont.ml>
2019-06-01 13:38:22 -0300

    Merge remote-tracking branch 'origin/master'

--- lib/screens/detai_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Detalhe feed'),
+      ),
+      body: Text('Detalhe feed'),
+    );
+  }
+}
