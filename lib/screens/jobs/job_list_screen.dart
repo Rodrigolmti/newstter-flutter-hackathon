a45c2a4e278498ae57df56aa8289d8571f2be46b
Rodrigo Lopes <rodrigolmti@gmail.com>
2019-06-01 16:30:28 -0300

    Fix background job color

--- lib/screens/jobs/job_list_screen.dart
+++ lib/screens/jobs/job_list_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:newstter/Models/Jobs/Jobs.dart';
 import 'package:newstter/screens/jobs/job_row.dart';
-import 'package:newstter/Theme.dart' as Theme;
 
 class JobList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Flexible(
       child: Container(
-        color: Theme.Colors.jobPageBackground,
         child: ListView.builder(
           itemExtent: 120.0,
           itemCount: JobDao.jobs.length,
