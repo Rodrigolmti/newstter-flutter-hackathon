af895cc998353fedbebac12626318f28c01cdfbd
Rodrigo Lopes <rodrigolmti@gmail.com>
2019-06-01 10:47:20 -0300

    Organize app files and create home screen

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+class NewstterApp extends StatefulWidget {
+  @override
+  _NewstterAppState createState() => _NewstterAppState();
+}
+
+class _NewstterAppState extends State<NewstterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: HomeScreen());
+  }
+}
